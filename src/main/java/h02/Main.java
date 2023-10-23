5eb1389ea0b8b29f455647582cae6ed4424a06e8
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-10-23 22:49:37 +0200

    Add smaller example, create mysterious pattern method

--- src/main/java/h02/Main.java
+++ src/main/java/h02/Main.java
@@ -14,11 +14,61 @@ public class Main {
      */
     public static void main(String[] args) {
         // define world dimensions
-        final int numberOfRows = 15;
-        final int numberOfColumns = 16;
+        int numberOfRows = 15;
+        int numberOfColumns = 16;
         // create an array for filling the world with some coins
         int[][] coins = new int[numberOfRows][numberOfColumns];
-        // add some entries to the array
+        // add some entries to the array using the provided method that provides a mysterious pattern
+        fillArray(coins);
+
+        // // for a smaller example, simply uncomment the following code
+        // numberOfRows = 6;
+        // numberOfColumns = 11;
+        // coins = new int[numberOfRows][numberOfColumns];
+        // coins[4][7] = 1;
+        // coins[1][8] = 1;
+        // coins[2][9] = 2;
+
+        // create a control center
+        ControlCenter controlCenter = new ControlCenter();
+        // set the world dimensions
+        World.setSize(numberOfColumns, numberOfRows);
+        // don't draw turned off robots
+        World.getGlobalWorld().setDrawTurnedOffRobots(false);
+        // open world, set its delay and draw some decorations
+        World.setVisible(true);
+        World.setDelay(30); // adjust this value if the world runs too fast/slow for your liking
+        World.placeBlock(0, 0);
+        World.placeHorizontalWall(0, 0);
+        World.placeVerticalWall(0, 0);
+        // use the previously created array to fill the world with coins
+        placeCoinsInWorld(coins);
+        // let the robots clean up the mess
+        controlCenter.cleanWorld();
+    }
+
+    /**
+     * Places coins in the world according to the provided array
+     *
+     * @param coins An array detailing how many coins to place in what position
+     */
+    public static void placeCoinsInWorld(int[][] coins) {
+        for (int y = 0; y < coins.length; y++) {
+            for (int x = 0; x < coins[y].length; x++) {
+                if (coins[y][x] != 0) {
+                    World.putCoins(x, y, coins[y][x]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Fills the given array with a mysterious pattern that represents coins in the world.
+     * The array should be at least 13x13 cells big.
+     *
+     * @param coins the array to fill
+     */
+    public static void fillArray(int[][] coins) {
         coins[12][11] = 3;
         coins[12][4] = 3;
         coins[11][10] = 3;
@@ -67,36 +117,5 @@ public class Main {
         coins[8][13] = 1;
         coins[9][13] = 1;
         coins[10][13] = 2;
-        // create a control center
-        ControlCenter controlCenter = new ControlCenter();
-        // set the world dimensions
-        World.setSize(numberOfColumns, numberOfRows);
-        // don't draw turned off robots
-        World.getGlobalWorld().setDrawTurnedOffRobots(false);
-        // open world, set its delay and draw some decorations
-        World.setVisible(true);
-        World.setDelay(2);
-        World.placeBlock(0, 0);
-        World.placeHorizontalWall(0, 0);
-        World.placeVerticalWall(0, 0);
-        // use the previously created array to fill the world with coins
-        placeCoinsInWorld(coins);
-        // let the robots clean up the mess
-        controlCenter.cleanWorld();
-    }
-
-    /**
-     * Places coins in the world according to the provided array
-     *
-     * @param coins An array detailing how many coins to place in what position
-     */
-    public static void placeCoinsInWorld(int[][] coins) {
-        for (int y = 0; y < coins.length; y++) {
-            for (int x = 0; x < coins[y].length; x++) {
-                if (coins[y][x] != 0) {
-                    World.putCoins(x, y, coins[y][x]);
-                }
-            }
-        }
     }
 }
